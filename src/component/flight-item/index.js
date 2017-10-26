8e712c73480b0ecd8dc0caf291810727a9dc146c
Caleb <caleb.wells1@gmail.com>
2017-10-26 10:41:48 -0700

    updating

--- src/component/flight-item/index.js
+++ src/component/flight-item/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {
+  ascendingTimeFilter,
+  descendingTimeFilter,
   ascendingMainCabinFilter,
   descendingMainCabinFilter, 
   ascendingFirstClassFilter, 
@@ -24,15 +26,16 @@ class FlightItem extends React.Component{
       departureTimeFilter: 0,
     }
 
-    this.handleSort = this.handleSort.bind(this)
+    this.handleFirstClassSort = this.handleFirstClassSort.bind(this)
+    this.handleMainCabinSort = this.handleMainCabinSort.bind(this)
+    this.handleDepartureTimeSort = this.handleDepartureTimeSort.bind(this)
   }
 
   
   componentWillMount(){}
   
-  handleSort(){
+  handleFirstClassSort(){
     this.stateProperty = this.state.firstClassFilter ? 0 : 1
-    console.log('switching to:', this.stateProperty)    
     this.setState({
       firstClassFilter: this.stateProperty,
       mainCabinFilter: 0,
@@ -40,6 +43,26 @@ class FlightItem extends React.Component{
     }, function(){ 
       console.log('__NEW_SORTED_STATE', this.state)
     })    
+  }
+
+  handleMainCabinSort(){
+    this.stateProperty = this.state.MainCabinFilter ? 0 : 1
+    this.setState({
+      firstClassFilter: 0,
+      mainCabinFilter: this.stateProperty,
+      departureTimeFilter: 0,
+    }, function(){ 
+      console.log('__NEW_SORTED_STATE', this.state)
+    })    
+  }
+
+  handleDepartureTimeSort(){
+    this.stateProperty = this.state.departureTimeFilter ? 0 : 1
+    this.setState({
+      firstClassFilter: 0,
+      mainCabinFilter: 0,
+      departureTimeFilter: this.stateProperty,
+    }, () => console.log('__NEW_SORTED_STATE', this.state))    
   }
 
   render(){
@@ -53,18 +76,32 @@ class FlightItem extends React.Component{
               <Col sm={6} md={2}>Flight Number</Col>
               <Col sm={6} md={2}>
                 <Button 
-                  type="submit"
-                  name="firstClassFilter"
                   onClick={() => {
-                    this.handleSort()
+                    this.handleFirstClassSort()
                     this.state.firstClassFilter ? 
                       this.props.ascendingFirstClassFilter(this.props.flights)
                     : this.props.descendingFirstClassFilter(this.props.flights)
                     }
                   }>First Class:</Button>
               </Col>
-              <Col sm={6} md={2}><Button>Main Cabin</Button></Col>
-              <Col sm={6} md={2}><Button>Departure Time:</Button></Col>
+              <Col sm={6} md={2}>
+                <Button
+                  onClick={() => {
+                    this.handleMainCabinSort()
+                    this.state.mainCabinFilter ? 
+                      this.props.ascendingMainCabinFilter(this.props.flights)
+                    : this.props.descendingMainCabinFilter(this.props.flights)
+                    }
+                  }>Main Cabin</Button></Col>
+              <Col sm={6} md={2}>
+                <Button
+                  onClick={() => {
+                    this.handleDepartureTimeSort()
+                    this.state.departureTimeFilter ? 
+                      this.props.ascendingTimeFilter(this.props.flights)
+                    : this.props.descendingTimeFilter(this.props.flights)
+                    }
+                  }>Departure Time:</Button></Col>
             </Row>
           </Panel>
           <Panel>
@@ -82,7 +119,7 @@ class FlightItem extends React.Component{
           }
           </Panel>
         </Grid> 
-        : 'No flights found'
+        : (this.props.flight ? 'No flights found' : undefined)
       }
       </div>
 
@@ -96,10 +133,12 @@ let mapStateToProps = state => ({
 })
 
 let mapDispatchToProps = dispatch => ({
+  ascendingTimeFilter: flights => dispatch(ascendingTimeFilter(flights)),
+  descendingTimeFilter: flights => dispatch(descendingTimeFilter(flights)),
   ascendingFirstClassFilter: flights => dispatch(ascendingFirstClassFilter(flights)),
   descendingFirstClassFilter: flights => dispatch(descendingFirstClassFilter(flights)),
-  ascendingMainCabinClassFilter: flights => dispatch(ascendingMainCabinClassFilter(flights)),
-  descendingMainCabinClassFilter: flights => dispatch(descendingMainCabinClassFilter(flights)),
+  ascendingMainCabinFilter: flights => dispatch(ascendingMainCabinFilter(flights)),
+  descendingMainCabinFilter: flights => dispatch(descendingMainCabinFilter(flights)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(FlightItem)
